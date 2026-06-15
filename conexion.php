8738b22eb125b0bdbe5d3b470f880f0dd13a46a3
Sebastián Deschamps C. <sdeschamps@ing.ucsc.cl>
2026-06-15 19:58:08 -0400

    Fix: cambio a localhost en conexion

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
-    $conexion = mysqli_connect("localhost:3306","root","","sgppe"); // ("direccion", "usuario", "contraseña", "nombre base de datos")
+$conexion = mysqli_connect("localhost", "root", "", "sgppe");
 
-    if($conexion->connect_error){
-        die("Conexión fallida: " . $conn->connect_error);
-    }
+if (!$conexion) {
+    die("Conexión fallida: " . mysqli_connect_error());
+}
 ?>
