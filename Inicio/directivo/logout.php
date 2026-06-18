35da41f7170af91b16c04010e95bffbc569b64a0
Jean Fuentes <jeanpaulfuentes99@gmail.com>
2026-06-17 23:43:32 -0400

    Arreglo de cierre de sesión

--- Inicio/directivo/logout.php
+++ Inicio/directivo/logout.php
@@ -1,6 +1,11 @@
 <?php
-session_start();
+if (session_status() !== PHP_SESSION_ACTIVE) {
+    session_start();
+}
+
+session_unset();
 session_destroy();
 
-header("Location: index.php");
-exit;       
+header('Location: ../iniciar_sesion.php');
+exit;
+?>
